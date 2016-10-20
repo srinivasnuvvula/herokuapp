e66b7b7b78ae322e771937aafc4efdc2219e2ce9
srinivasnuvvula <snuvvula@murraystate.edu>
2016-10-20 02:31:33 +0000

    Gem File Changes with my name in it

--- chapter1/hello_app/app/controllers/application_controller.rb
+++ chapter1/hello_app/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-      render html: "Hello World!"
+      render html: "Hello World! This is from Srinivas Nuvvula Reddi"
   end
 end
